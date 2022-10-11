968a2c8f55df70569600af19a2cf520f408005dc
Olesya Schukina <olesyaschu@gmail.com>
2022-10-11 19:36:08 +0400

    Fix unused issue

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,21 @@
  * Арбитражный бот на сети Polygon
  * [Мой github с другими работами]{@link https://github.com/olesyaschu GitHub} <br/>
  */
-import { ethers } from 'ethers'
-import pairsQuick from './data/pairsQuick.json'
-import pairsUni3 from './data/pairsUni3.json'
+// import { ethers } from 'ethers'
+// import pairsQuick from './data/pairsQuick.json'
+// import pairsUni3 from './data/pairsUni3.json'
 
 /** Создаем провайдер для дальнейших манипуляций с блокчейн сетью */
-const provider = new ethers.providers.InfuraWebSocketProvider(
-  'matic',
-  process.env.INFURA_KEY
-)
+// const provider = new ethers.providers.InfuraWebSocketProvider(
+//   'matic',
+//   process.env.INFURA_KEY
+// )
 
 /** Создаем пары с указанием их бирж в отдельном поле */
-const quick = { name: 'quickswap', pairs: pairsQuick}
-const uni3 = { name: 'uniswap3', pairs: pairsUni3}
+// const quick = { name: 'quickswap', pairs: pairsQuick}
+// const uni3 = { name: 'uniswap3', pairs: pairsUni3}
 /** Создаем массив exchanges для получения trades */
-const exchanges = [quick, uni3]
+// const exchanges = [quick, uni3]
 
 interface Pair {
   address: string
